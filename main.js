3751bfe3f853fa06807218265160eaf0ae8c5134
Mohamed Rabee <166071170+Mohamerabee98@users.noreply.github.com>
2024-09-05 15:27:36 +0300

    butn change

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let button = document.querySelector('button');
+let button = document.querySelector('.btn12');
 let section = document.querySelector('.name');
 button.addEventListener('click', function(){
     section.classList.toggle('change-color');
