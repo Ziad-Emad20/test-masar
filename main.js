2985955883378b0c6099fee2a91e5232d0f48c5a
Ziad Emad <ziaemad0111@gmail.com>
2024-09-05 16:11:54 +0300

    add slide to page

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 let button = document.querySelector('.btn12');
 let section = document.querySelector('.name');
-button.addEventListener('click', function(){
+button.addEventListener('click', function () {
     section.classList.toggle('change-color');
+});
+
+
+
+// ! Initialize Swiper 
+
+const progressCircle = document.querySelector(".autoplay-progress svg");
+const progressContent = document.querySelector(".autoplay-progress span");
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 4500,
+        disableOnInteraction: false
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    on: {
+        autoplayTimeLeft(s, time, progress) {
+            progressCircle.style.setProperty("--progress", 1 - progress);
+            progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+        }
+    }
 });
